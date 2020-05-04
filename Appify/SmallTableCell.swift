90a7a875e5edab0c347936c154e93c7afc33c0c5
Javier Calderon Jr <javierjcjr@gmail.com>
2020-05-04 03:25:55 -0700

    set up last section

--- Appify/SmallTableCell.swift
+++ Appify/SmallTableCell.swift
@@ -8,6 +8,38 @@
 
 import UIKit
 
-class SmallTableCell: UICollectionViewCell {
+class SmallTableCell: UICollectionViewCell, SelfConfiguringCell {
+    static let reusableIdentifier: String = "SmallTableCell"
     
+    let name = UILabel()
+    let imageView = UIImageView()
+    
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        
+        name.font = UIFont.preferredFont(forTextStyle: .title2)
+        name.textColor = .label
+        
+        imageView.layer.cornerRadius = 5
+        imageView.clipsToBounds = true
+        
+        let stackView = UIStackView(arrangedSubviews: [imageView, name])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            stackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
+    
+    func configure(with app: App) {
+        name.text = app.name
+        imageView.image = UIImage(named: app.image)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
